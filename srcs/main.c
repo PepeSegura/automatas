bf078ba8c8150accab2aa5fcb04c1867844fdd8d
Pepe Segura <pepeseguracarreno@gmail.com>
2023-12-07 01:29:13 +0100

    need jorge

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: psegura- <psegura-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 20:04:21 by psegura-          #+#    #+#             */
-/*   Updated: 2023/12/06 00:16:28 by psegura-         ###   ########.fr       */
+/*   Updated: 2023/12/07 01:28:00 by psegura-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,18 @@ const char *s_states[] =
 	NULL
 };
 
-const char *dic = "01";
+const char *dic = "-01";
 
 int	get_state(int x, int y)
 {
 	const int	states[][4] = {
-//   0  1 \0  
-	{2, 2, 3, 1}, //Empty
-	{1, 2, 3, 1}, //Error
-	{2, 2, 3, 1}, //0
-	{3, 2, 3, 1}  //1
+//   *  0  1 \0  
+	{1, 2, 3, 1}, //Empty
+	{1, 2, 2, 1}, //Error
+	{1, 2, 2, 1}, //0
+	{1, 2, 3, 1}  //1
 	};
+	printf("X: [%d] Y: [%d] -> State: [%d] [%s]\n", x, y, states[x][y], s_states[x]);
 	return (states[x][y]);
 }
 
@@ -39,10 +40,13 @@ int	get_index(char c)
 {
 	int i = 0;
 
+	if (ft_isalpha(c))
+		return (3);
+
 	while (dic[i])
 	{
 		if (dic[i] == c)
-			return (i+1);
+			return (i);
 		i++;
 	}
 	return (0);
@@ -51,8 +55,8 @@ int	get_index(char c)
 int get_number(char c)
 {
 	if (ft_isdigit(c))
-		return (c - 48);
-	return (c);
+		return (c - '0');
+	return (0);
 }
 
 int	evaluate(char *str)
@@ -62,8 +66,9 @@ int	evaluate(char *str)
 
 	while (str[i])
 	{
-		res = get_state(get_index(str[i]), str[i] - 48);
-		printf("state: [%d]\n", res);
+		printf("%c -> ", str[i]);
+		res = get_state(get_index(str[i]), get_number(str[i]));
+		// printf("state: [%d]\n", res);
 		i++;
 	}
 	if (res < 2)
@@ -76,7 +81,7 @@ int	evaluate(char *str)
 
 int	main(void)
 {
-	char	*str = "1a0001";
+	char	*str = "1aa0001";
 	int		state = evaluate(str);
 
 	printf("Input: [%s]\n", str);
